61c9ee2e99c46f189c75625c7007a687c2e1cc46
rmdhnptr22 <ramadhaniyuditama30295@gmail.com>
2025-07-17 23:22:29 +0700

    upate isi file

--- js/script.js
+++ js/script.js
@@ -21,3 +21,41 @@ window.onbeforeunload = () => {
         form.reset();
     }
 }
+
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Koneksi PostgreSQL
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost', 
+  database: 'Db_Portofolio',
+  password: 'jombangsantri22', 
+  port: 5432,
+});
+
+// Endpoint menerima pesan dari form
+app.post('/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    await pool.query(
+     'INSERT INTO contacts (name, email, message) VALUES ($1, $2, $3)',
+      [name, email, message]
+    );
+    res.status(200).send('Pesan berhasil disimpan');
+  } catch (err) {
+    console.error('Database error:', err);
+    res.status(500).send('Terjadi kesalahan saat menyimpan data');
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
